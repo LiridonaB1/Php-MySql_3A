1ac71c119370592a80ff74131c27da7d1bc81ca0
LiridonaB1 <liridona.baliu@digitalschool.tech>
2026-09-09 16:20:41 +0200

    mdoule3

---
+++ Module3/index.php
@@ -0,0 +1,72 @@
+<?php
+
+$num = 4;
+
+if($num >0){
+    echo "$num is greater than 0";
+}
+
+
+$age = 14;
+
+echo "<br>";
+if(($age > 12) && ($age < 20)) {
+    echo "You are a teenager";
+}
+
+
+echo "<br>";
+$age = 19;
+if ($age < 18) {
+    echo "You are under 18";
+}else{
+    echo "You are an adult";
+}
+
+echo "<br>";
+
+$number = 5;
+if($number < 0){
+    echo "The value of $number is a negative number";
+}elseif($number == 0){
+    echo  "The value of $number is a zero";
+}else {
+    echo "The value of $number is a positive number";
+}
+
+echo "<br>";
+
+$dita =1;
+
+switch($dita) {
+
+case 1:
+     echo "E hene";
+     break;
+case 2: 
+    echo "E marte";
+     break;
+case 3: 
+    echo "E merkure";
+     break;
+
+default:
+echo "Dite e pavlefshme";
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+?>
