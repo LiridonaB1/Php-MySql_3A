4d2314dcd508db3aaeebad13d762ec3f643934d3
LiridonaB1 <liridona.baliu@digitalschool.tech>
2026-09-14 16:04:50 +0200

    added different loops

--- Module3/index.php
+++ Module3/index.php
@@ -53,20 +53,48 @@ case 3:
 default:
 echo "Dite e pavlefshme";
 
-
-
-
+}
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+//Loops
+ //1. While loop
+$x = 1;
+while($x <= 5) {
+    echo "The number is : $x <br>";
+    $x++;
 }
 
+ //2.Do while loop
+ echo "<br>";
+ echo "<br>";
 
+    $y = 1;
+ do {
+    echo "The number is : $y <br>";
+    $y++;
+ }while($y >= 5);
 
+ //3.For loop
+ for($x=0; $x <=10;$x++){
+       echo "Numri eshte: $x <br>";
+ }
+  echo "<br>";
 
+ //4.Foreach loop - works only on arrays
 
+  $cars = array("BMW","Vw","Audi","Tesla");
 
+  foreach($cars as $value){
+    echo "$value <br>";
+  }
 
-
-
-
+  
+$age = array("John" => 18, "Michael" => 20, "Joe" => 13);
+ foreach($age as $key => $value){
+    echo "$key = $value  <br>";
+ }
 
 
 ?>
