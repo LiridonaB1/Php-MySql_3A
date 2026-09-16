daa456ce350b78fd6b4bda4a6010673897f56208
LiridonaB1 <liridona.baliu@digitalschool.tech>
2026-09-16 16:08:37 +0200

    lesson 4

---
+++ Module4/index.php
@@ -0,0 +1,103 @@
+<?php
+
+
+  //Built-in Functions
+
+ //phpinfo();
+
+ $x = "Hello";
+ print_r($x);
+
+ echo "<br>";
+
+
+ $a = 5;
+ echo gettype($a) . "<br>";
+
+ $b = 10.3;
+ echo gettype($b) . "<br>";
+
+ $c = "Hi";
+ echo gettype($c) . "<br>";
+
+//User-defined functions
+
+ function display(){
+    echo "this is PHP version: " . phpversion();
+    echo "<br>";
+ }
+
+ display();
+
+function hello(){
+    echo "Hello World";
+    echo "<br>";
+}
+hello();
+
+function sum(){
+    $value = 120 + 20;
+    echo $value;
+    echo "<br>";
+}
+
+sum();
+
+
+function shuma($x,$y){
+    $value = $x + $y;
+    echo $value;
+    echo "<br>";
+
+}
+
+
+shuma(12,40);
+
+
+function maximum($a,$b) {
+    if($a > $b){
+        return $a;
+    }else{
+        return $b;
+    }
+
+}
+ $a = 10;
+ $b = 30;
+
+$test = maximum(10,30);
+echo "The max of $a and $b is $test";
+echo "<br>";
+
+
+$z = 5;//global variable
+
+function localVariable() {
+    $h = 10; // local variable
+    //echo $z;
+    echo $h;
+    global $z;
+    echo "<br>";
+    echo $z;
+
+}
+
+localVariable();
+
+echo "<br>";
+
+
+
+function callCounter(){
+    static $count = 0;
+    $count++; //1
+    echo "The value of count is: $count";
+    echo "<br>";
+}
+
+callCounter(); //count=1
+callCounter();
+
+
+?>
